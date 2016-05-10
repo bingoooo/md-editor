9fca7a1d2765db6e65259442a14f26ab82fc65a5
Dant Benjamin <sukkhato@gmail.com>
2016-05-10 16:45:02 +0200

    modification de la page d'acceuil par la page de l'éditeur

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,22 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Hi there</title>
+	<title>md-editor</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="css/codemirror.css">
+	<link rel="stylesheet" href="css/style.css">
+	<script src="js/codemirror.js"></script>
+	<script src="js/markdown.js"></script>
+	<script src="js/react.js"></script>
+	<script src="js/react-dom.js"></script>
+	<script src="js/browser.min.js"></script>
+	<script src="js/marked.min.js"></script>
+	<script src="js/script.js"></script>
 </head>
 <body>
-	<h1>Test</h1>
+	<div id="md-field"></div>
+	<div id="code"></div>
+	<div id="mirror"></div>
+	<div id="html-processed"></div>
 </body>
 </html>
