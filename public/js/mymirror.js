c119a31db342be512dadd6be0960ab27e3d5d6ee
Dant Benjamin <sukkhato@gmail.com>
2016-05-11 11:50:56 +0200

    Fichier JS utilisant code mirror

--- public/js/mymirror.js
+++ public/js/mymirror.js
@@ -4,3 +4,38 @@ console.log('sourced : mymirror.js');
 
 var user;
 var users;
+
+var mdEditor = {
+	input: '',
+	output: '',
+	init: function(){
+		this.input = document.getElementById('code');
+		this.output = document.getElementById('mirror');
+		console.log(this.input, this.output);
+		this.mirroring();
+	},
+	mirroring: function(){
+		self = this;
+		var code = CodeMirror(this.input, {
+			value: '# Test',
+			mode: 'markdown',
+		});
+		code.on('change', function(cm){
+			var lines = cm.doc.children[0].lines;
+			var out = '';
+			if(lines.length){
+				for (var i = 0; i < lines.length; i++) {
+					console.log('lines :', lines[i].text);
+					out += lines[i].text + '\n';
+				}
+			}
+			var processed = marked(out, {sanitized: true});
+			console.log(processed);
+			self.output.innerHTML = processed;
+		});
+	}
+}
+
+window.onload = function(){
+	mdEditor.init();
+}
