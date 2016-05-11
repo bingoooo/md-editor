743d211d447f3926d758dc5a366c8b63501ac1df
Dant Benjamin <sukkhato@gmail.com>
2016-05-11 11:52:12 +0200

    Ajout de la vue éditeur avec angular 2

---
+++ resources/views/angular.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Markdown Editor : With Angular 2</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="css/codemirror.css">
+	<link rel="stylesheet" href="css/style.css">
+	<script src="js/codemirror.js"></script>
+	<script src="js/markdown.js"></script>
+</head>
+<body>
+<nav>
+	<ul>
+		<li><a href="/home">Code Mirror if I</a></li>
+		<li><a href="/react">React if I</a></li>
+		<li><a href="/angular">A2 if I</a></li>
+	</ul>
+</nav>
+	<div id="md-field"></div>
+	<div id="code"></div>
+	<div id="mirror"></div>
+	<div id="html-processed"></div>
+</body>
+</html>
