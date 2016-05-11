022a53072c17b0bc249b4ed5f4ba7be885101843
Dant Benjamin <sukkhato@gmail.com>
2016-05-11 11:51:47 +0200

    Réorganisation de la page d'acceuil pour recevoir le style

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,26 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Markdown Editor</title>
+	<title>Markdown Editor : With Code Mirror</title>
 	<meta charset="utf-8">
 	<link rel="stylesheet" href="css/codemirror.css">
 	<link rel="stylesheet" href="css/style.css">
 	<script src="js/codemirror.js"></script>
 	<script src="js/markdown.js"></script>
-	<script src="js/react.js"></script>
-	<script src="js/react-dom.js"></script>
-	<script src="js/browser.min.js"></script>
 	<script src="js/marked.min.js"></script>
 	<script src="js/mymirror.js"></script>
-	<script src="js/script.js"></script>
 </head>
 <body>
-	<div id="md-field"></div>
-	<div id="code"></div>
-	<div id="mirror"></div>
-	<div id="html-processed"></div>
+<nav>
+	<ul>
+		<li><a href="/home">Code Mirror if I</a></li>
+		<li><a href="/react">React if I</a></li>
+		<li><a href="/angular">A2 if I</a></li>
+	</ul>
+</nav>
+	<div id="md-field">
+		<div id="code"></div>
+		<div id="mirror"></div>
+	</div>
 </body>
 </html>
