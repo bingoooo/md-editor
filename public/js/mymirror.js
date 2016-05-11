4c26bd9b4d75b269a1f66a42219b0f90b5a94a46
Dant Benjamin <sukkhato@gmail.com>
2016-05-11 14:20:42 +0200

    suppression des console.log inutiles

--- public/js/mymirror.js
+++ public/js/mymirror.js
@@ -11,7 +11,6 @@ var mdEditor = {
 	init: function(){
 		this.input = document.getElementById('code');
 		this.output = document.getElementById('mirror');
-		console.log(this.input, this.output);
 		this.mirroring();
 	},
 	mirroring: function(){
@@ -25,12 +24,10 @@ var mdEditor = {
 			var out = '';
 			if(lines.length){
 				for (var i = 0; i < lines.length; i++) {
-					console.log('lines :', lines[i].text);
 					out += lines[i].text + '\n';
 				}
 			}
 			var processed = marked(out, {sanitized: true});
-			console.log(processed);
 			self.output.innerHTML = processed;
 		});
 	}
