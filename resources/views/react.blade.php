558c61b45f3295dc7db5723cc243a543c8550907
Dant Benjamin <sukkhato@gmail.com>
2016-05-11 11:52:44 +0200

    Ajout de la vue éditeur avec react

---
+++ resources/views/react.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Markdown Editor : With React</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="css/codemirror.css">
+	<link rel="stylesheet" href="css/style.css">
+	<script src="js/codemirror.js"></script>
+	<script src="js/markdown.js"></script>
+	<script src="js/react.js"></script>
+	<script src="js/react-dom.js"></script>
+	<script src="js/browser.min.js"></script>
+	<script src="js/marked.min.js"></script>
+	<script src="js/mymirror.js"></script>
+	<script src="js/script.js"></script>
+</head>
+<body>
+<nav>
+	<ul>
+		<li><a href="/home">Code Mirror if I</a></li>
+		<li><a href="/react">React if I</a></li>
+		<li><a href="/angular">A2 if I</a></li>
+	</ul>
+</nav>
+	<div id="md-field"></div>
+	<div id="code"></div>
+	<div id="mirror"></div>
+	<div id="html-processed"></div>
+</body>
+</html>
