cfc0e24f0edff39789c74cb29b35ff3fd0e8de81
Dant Benjamin <sukkhato@gmail.com>
2016-05-12 16:47:58 +0200

    Modification du controlleur pour acquérir les requêtes fetch

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -3,8 +3,33 @@
 namespace App\Http\Controllers;
 
 use Laravel\Lumen\Routing\Controller as BaseController;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use App\Markdown;
 
 class Controller extends BaseController
 {
     //
+	public function getFiles(){
+		$datas = Markdown::all();
+		return $datas;
+	}
+
+	public function getFile($id){
+		$file = Markdown::find($id)->get();
+		return $file;
+	}
+
+    public function postFile(Request $req){
+    	$exist = true;
+    	$check = Markdown::where('title', $req->input('title'))->first();
+    	$markdown = $check?$check:new Markdown;
+    	if($markdown->title == null) {
+	    	$markdown->title = $req->input('title');
+	    	$exist = false;
+    	}
+	    $markdown->markdown = $req->markdown;
+	    // $markdown->save();
+    	return response(['name' => $markdown])->header('Existing-file', $exist);
+    }
 }
